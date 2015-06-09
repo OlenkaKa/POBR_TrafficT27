109b47115d6e8b88a73c278c1af00becfc8e7c1a
Aleksandra Karbarczyk <akarbarczyk@gmail.com>
2015-06-09 23:52:02 +0200

    Segmentation for many labels

--- include/CommonFunctions.h
+++ include/CommonFunctions.h
@@ -5,6 +5,7 @@
 
 void convertHSV(const cv::Mat& image);
 cv::Mat indexSegments(const cv::Mat& image);
+bool isLess(const cv::Vec3f& v1, const cv::Vec3f& v2);
 cv::Mat selectColor(const cv::Mat& image, const cv::Vec3b& minValues, const cv::Vec3b& maxValues);
 
 #endif /* COMMONFUNCTIONS_H_ */
